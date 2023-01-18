53fbf28a50343eb746a2cb1dafe214ff5afb29d9
cafe small house <ybaram40@gmail.com>
2023-01-19 07:54:59 +0900

    styled-component 설정 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const Container = styled.div`
+   ${tw`min-h-full bg-red-500 h-full`}
+`;
+
+const Content = styled.div`
+   ${tw`text-xl text-white`}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <Container>
+         <Content>123</Content>
+      </Container>
+   );
 }
 
 export default App;
